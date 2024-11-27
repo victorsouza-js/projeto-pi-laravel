5ebab27e618fd2a58599adf2ae3b73c0d472e71b
victorsouza <victorsouzaaa5611@gmail.com>
2024-11-27 20:29:32 -0300

    add table

--- projeto-pi/database/migrations/2024_11_27_190022_create_nome_table.php
+++ projeto-pi/database/migrations/2024_11_27_190022_create_nome_table.php
@@ -13,47 +13,49 @@ return new class extends Migration
     {
         Schema::create('produto', function (Blueprint $table) {
             $table->id();
-            $table -> string('nome');
-            $table -> string('preco');
-            $table -> string('qtd_estoque');
+            $table ->string('nome');
+            $table ->string('preco');
+            $table ->string('qtd_estoque');
         });
 
         Schema::create('vendas', function (Blueprint $table) {
             $table->id();
-            $table -> string('valor_total');
-            $table -> string('data');
-            $table -> string('cliente_id');
-            $table -> string('vendedor_id');
-            $table -> string('pagamento_id');
+            $table ->decimal('valor_total');
+            $table ->dateTime('data');
+            $table ->integer('cliente_id');
+            $table ->integer('vendedor_id');
+            $table ->integer('pagamento_id');
         });
 
 
         Schema::create('cliente', function (Blueprint $table) {
             $table->id();
-            $table -> string('nome');
-            $table -> string('telefone');
-            $table -> string('endereco');
-            $table -> string('cpf');
-            $table -> string('email');
-            $table -> string('cep');
+            $table ->string('nome');
+            $table ->integer('telefone');
+            $table ->string('endereco');
+            $table ->string('cpf');
+            $table ->string('email');
+            $table ->string('cep');
         });
 
         Schema::create('pagamento', function (Blueprint $table) {
             $table->id();
-            $table -> string('forma_pagamento');
+            $table ->string('forma_pagamento');
         });
 
 
         Schema::create('categoria', function (Blueprint $table) {
             $table->id();
-            $table -> string('nome');
+            $table ->string('nome');
         });
 
         Schema::create('fornecedores', function (Blueprint $table) {
             $table->id();
-            $table -> string('cnpj');
-            $table -> string('nome');
+            $table ->string('cnpj');
+            $table ->string('nome');
         });
+
+
 
 
 
