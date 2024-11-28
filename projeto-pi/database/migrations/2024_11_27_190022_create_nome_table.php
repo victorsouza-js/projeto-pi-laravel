621fac4aee4729f44d2ca648a97fd33cadb2d999
victorsouza-git <victorsouzaaa5611@gmail.com>
2024-11-27 22:50:02 -0300

    add

--- projeto-pi/database/migrations/2024_11_27_190022_create_nome_table.php
+++ projeto-pi/database/migrations/2024_11_27_190022_create_nome_table.php
@@ -11,9 +11,48 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('nome', function (Blueprint $table) {
+        Schema::create('produto', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table ->string('nome');
+            $table ->string('preco');
+            $table ->string('qtd_estoque');
+        });
+
+        Schema::create('vendas', function (Blueprint $table) {
+            $table->id();
+            $table ->decimal('valor_total');
+            $table ->dateTime('data');
+            $table ->integer('cliente_id');
+            $table ->integer('vendedor_id');
+            $table ->integer('pagamento_id');
+        });
+
+
+        Schema::create('cliente', function (Blueprint $table) {
+            $table->id();
+            $table ->string('nome');
+            $table ->integer('telefone');
+            $table ->string('endereco');
+            $table ->string('cpf');
+            $table ->string('email');
+            $table ->string('cep');
+        });
+
+        Schema::create('pagamento', function (Blueprint $table) {
+            $table->id();
+            $table ->string('forma_pagamento');
+        });
+
+
+        Schema::create('categoria', function (Blueprint $table) {
+            $table->id();
+            $table ->string('nome');
+        });
+
+        Schema::create('fornecedores', function (Blueprint $table) {
+            $table->id();
+            $table ->string('cnpj');
+            $table ->string('nome');
         });
     }
 
