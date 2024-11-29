3e021d82b7e4d679b1730433afa6e8e2ce6649d5
victorsouza-git <victorsouzaaa5611@gmail.com>
2024-11-29 06:52:24 -0300

    add

--- projeto-pi/database/seeders/ProdutoSeeder.php
+++ projeto-pi/database/seeders/ProdutoSeeder.php
@@ -11,14 +11,14 @@ class ProdutoSeeder extends Seeder
     {
         DB::table('produto')->insert([
             [
-                'nome' => 'Creatina 300g',
-                'preco' => '72.90',
-                'qtd_estoque' => '100',
+                'nome' => 'Creatina Dark Lab 300g',
+                'preco' => '73.00',
+                'qtd_estoque' => '670',
             ],
             [
                 'nome' => 'Whey protein 1kg',
                 'preco' => '103.90',
-                'qtd_estoque' => '60',
+                'qtd_estoque' => '230',
             ],
             [
                 'nome' => 'Vitamina C 500mg',
@@ -28,14 +28,23 @@ class ProdutoSeeder extends Seeder
             [
                 'nome' => 'Creatina Max 300mg',
                 'preco' => '83.00',
-                'qtd_estoque' => '70',
+                'qtd_estoque' => '340',
             ],
             [
                 'nome' => 'Creatina Growth 300g',
                 'preco' => '72.00',
                 'qtd_estoque' => '150',
             ],
-           
+            [
+                'nome' => 'Creatina Integral Médica 300g',
+                'preco' => '71.00',
+                'qtd_estoque' => '170',
+            ],
+            [
+                'nome' => 'Pré Treino Insany Clow',
+                'preco' => '124.00',
+                'qtd_estoque' => '245',
+            ],
             
         ]);
     }
