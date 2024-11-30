10ea4012657735eee15eeb67a8f04eee897b7b0a
victorsouza-git <victorsouzaaa5611@gmail.com>
2024-11-29 23:39:11 -0300

    add

---
+++ projeto-pi/resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('title', 'Home - PowerUp Suplementos')
+
+@section('content')
+   <h1>AAIIIIII</h1>
+@endsection
---
+++ projeto-pi/resources/views/layouts/app.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="shortcut icon" type="x-icon" href="{{ asset('assets/imgs/logo.png') }}">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title', 'PowerUp Suplementos')</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('assets/css/index.css') }}">
+</head>
+<body>
+   
+
+    <main>
+        @yield('content')
+    </main>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+</body>
+</html>
